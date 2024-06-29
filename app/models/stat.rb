54464f0e8db4736a7db6144da60b4de5dd8d6dff
Zebler <94908862+Zebler@users.noreply.github.com>
2024-06-29 14:00:51 +0700

    add image_process, fixed stats model

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,24 +1,7 @@
 class Stat < ApplicationRecord:Base
   belongs_to :champion , class_name: "Champion", foreign_key: "Champ_id"
 end
-class Stat
-  integer Level
-  integer Health
-  integer Health_growth
-  integer Health_regen
-  integer Health_regen_growth
-  integer Resource
-  integer Resource_growth
-  integer Resource_regen
-  integer Resource_regen_growth
-  integer Armor
-  integer Armor_growth
-  integer MS
-  integer MS_growth
-  integer AD
-  integer AD_growth
-end
-class Stat
+attr_accessor :Level, :Health, :Health_growth, :Health_regen, :Health_regen_growth, :Resource, :Resource_growth, :Resource_regen, :Resource_regen_growth, :Armor, :Armor_growth, :MS, :MS_growth, :AD, :AD_growth
   @@no_of_Stat = 0
   def initialize(Level, Health, Health_growth, Health_regen, Health_regen_growth, Resource, Resource_growth, Resource_regen, Resource_regen_growth, Armor, Armor_growth, MS, MS_growth, AD, AD_growth)
     @Level = Level
@@ -28,6 +11,8 @@ class Stat
     @Health_regen_growth = Health_regen_growth
     @Resource = Resource
     @Resource_growth = Resource_growth
+    @Resource_regen = Resource_regen
+    @Resource_regen_growth = Resource_regen_growth
     @Armor = Armor
     @Armor_growth = Armor_growth
     @MS = MS
